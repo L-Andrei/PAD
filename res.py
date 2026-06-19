c91bd73e5031933ceea3fe793f3f973042c69888
L-Andrei <lucas23marin@gmail.com>
2026-06-19 00:00:03 -0300

    adicionando o script python para gerar as médias e medianas

---
+++ res.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Carrega o arquivo com os dados
+df = pd.read_csv('resultados_benchmark.csv')
+
+# Calcula a média e a mediana de cada uma das coluna
+resultados = pd.DataFrame({
+    'Média': df.mean(),
+    'Mediana': df.median()
+})
+
+print(resultados)
